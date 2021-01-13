a61979dccf1e08c7b0699134971a46ddef9616c0
Fynn Maarten <fynn@Fynns-iMac.local>
2021-01-13 12:06:27 -0800

    moved Convert helper to plugin code

--- ios/Classes/TorchBridge/TorchModule.mm
+++ ios/Classes/TorchBridge/TorchModule.mm
@@ -1,5 +1,8 @@
 #import "TorchModule.h"
-#import "Convert.h"
+//#import "../Helpers/Convert.mm"
+#import <LibTorch/LibTorch.h>
+
+@class Convert;
 
 @implementation TorchModule {
 @protected
@@ -50,7 +53,7 @@
     for(int i = 0; i < [shape count]; i++){
         shapeVec.push_back([[shape objectAtIndex:i] intValue]);
     }
-    at::ScalarType type = [Convert dtypeFromString:dtype];
+    at::ScalarType type = [self _convert: dtype];
     
     at::Tensor tensor = torch::from_blob(data, shapeVec, type);
     torch::autograd::AutoGradMode guard(false);
@@ -71,6 +74,24 @@
     return [results copy];
 }
 
-
+- (at::ScalarType)_convert:(NSString*)dtype {
+    NSArray *dtypes = @[@"float32", @"float64", @"int32", @"int64", @"int8", @"uint8"];
+    int type = (int)[dtypes indexOfObject:dtype];    
+    switch(type){
+    case 0:
+        return torch::kFloat32;
+    case 1:
+        return torch::kFloat64;
+    case 2:
+        return torch::kInt32;
+    case 3:
+        return torch::kInt64;
+    case 4:
+        return torch::kInt8;
+    case 5:
+        return torch::kUInt8;
+    }
+    return at::ScalarType::Undefined;
+}
 
 @end
